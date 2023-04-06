e356aee8a70622ccd5155cc7b0bc50ac498a3ba9
zinwang <chriswangxxxxx@gmail.com>
2023-04-06 10:38:39 +0800

    add code and data for testing autogen feature

--- tune.py
+++ tune.py
@@ -2,13 +2,21 @@ from PETWorks.autotune import autoTune
 from PETWorks.attributetypes import QUASI_IDENTIFIER
 from PETWorks import report
 
-originalData = "data/delta_test4.csv"
-#originalData = "data/delta.csv"
+originalData = "data/adult10.csv"
+#originalData = "data/adult100.csv"
+#originalData = "data/adult1000.csv"
 
 
 attributeTypes = {
     "zip": QUASI_IDENTIFIER,
     "age": QUASI_IDENTIFIER,
+    "race": QUASI_IDENTIFIER,
+    "marital-status": QUASI_IDENTIFIER,
+    "education": QUASI_IDENTIFIER,
+    "native-country": QUASI_IDENTIFIER,
+    "workclass": QUASI_IDENTIFIER,
+    "occupation": QUASI_IDENTIFIER,
+    "salary-class": QUASI_IDENTIFIER,
 }
 
 
@@ -17,5 +25,5 @@ def analyzingFunction(anonymizedData):
     return data.astype(float).mean()
 
 
-result = autoTune(originalData, attributeTypes, analyzingFunction, 0.5, 7)
+result = autoTune(originalData, attributeTypes, analyzingFunction, 0.5, 64)
 report(result, "json")
