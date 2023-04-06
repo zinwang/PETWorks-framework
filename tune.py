7c3177b7e28667bd1189a40faaf0100e369f2a37
zinwang <chriswangxxxxx@gmail.com>
2023-04-06 20:06:59 +0800

    ignore hierarchies generation

--- tune.py
+++ tune.py
@@ -6,17 +6,18 @@ originalData = "data/adult10.csv"
 #originalData = "data/adult100.csv"
 #originalData = "data/adult1000.csv"
 
+dataHierarchy = "data/adult_hierarchy"
 
 attributeTypes = {
-    "sex": QUASI_IDENTIFIER,
+#    "sex": QUASI_IDENTIFIER,
     "age": QUASI_IDENTIFIER,
-    "race": QUASI_IDENTIFIER,
-    "marital-status": QUASI_IDENTIFIER,
-    "education": QUASI_IDENTIFIER,
-    "native-country": QUASI_IDENTIFIER,
-    "workclass": QUASI_IDENTIFIER,
-    "occupation": QUASI_IDENTIFIER,
-    "salary-class": QUASI_IDENTIFIER,
+#    "race": QUASI_IDENTIFIER,
+#    "marital-status": QUASI_IDENTIFIER,
+#    "education": QUASI_IDENTIFIER,
+#    "native-country": QUASI_IDENTIFIER,
+#    "workclass": QUASI_IDENTIFIER,
+#    "occupation": QUASI_IDENTIFIER,
+#    "salary-class": QUASI_IDENTIFIER,
 }
 
 
@@ -25,5 +26,5 @@ def analyzingFunction(anonymizedData):
     return data.astype(float).mean()
 
 
-result = autoTune(originalData, attributeTypes, analyzingFunction, 0.5, 64)
+result = autoTune(originalData, dataHierarchy, attributeTypes, analyzingFunction, 0.5, 64)
 report(result, "json")
